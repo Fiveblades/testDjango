d5769c9a006a36f6c3e78f19913948010f7c4324
Fiveblades <fiveblades@gmail.com>
2024-04-13 14:36:37 +0200

    versie 14

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -117,11 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-static_root = "/home/Fiveblades/fiveblades.eu.pythonanywhere.com/mysite/static"
